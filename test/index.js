6029a3d3c53ebcd2b90dce50e2e45024a46d2422
Rod Vagg <rod@vagg.org>
2022-02-15 15:17:24 +1100

    chore: switch to mocha, use polendina for browser tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,18 @@
-const test = require('tape').test
+/* globals it */
+
 const vectors = require('hash-test-vectors')
 const { base64 } = require('multiformats/bases/base64')
-
+const { assert } = require('chai')
 const RIPEMD160 = require('../')
 
-vectors.forEach(function (vector, i) {
+vectors.forEach((vector, i) => {
   const input = base64.baseDecode(vector.input)
 
-  test('vector #' + (i + 1) + '', function (t) {
-    t.same(new RIPEMD160().update(input).digest('hex'), vector.ripemd160)
-    t.end()
+  it('vector #' + (i + 1) + '', () => {
+    assert.strictEqual(new RIPEMD160().update(input).digest('hex'), vector.ripemd160)
   })
 })
 
-test('42', function (t) {
-  t.same(new RIPEMD160().update('42').digest('hex'), '0df020ba32aa9b8b904471ff582ce6b579bf8bc8')
-  t.end()
+it('42', () => {
+  assert.strictEqual(new RIPEMD160().update('42').digest('hex'), '0df020ba32aa9b8b904471ff582ce6b579bf8bc8')
 })
